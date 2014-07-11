7f052232feb950ad4902467aa918625f35f7f9ca
Georgy Angelov <georgyangelov@gmail.com>
2014-07-11 20:29:04 +0300

    Add parenExpression tests

---
+++ src/test/java/net/gangelov/parser/ParenthesizedExpressionTest.java
@@ -0,0 +1,21 @@
+package net.gangelov.parser;
+
+import org.junit.Test;
+
+public class ParenthesizedExpressionTest {
+    @Test
+    public void testParenthesizedConstExpression() throws Exception {
+        XParserTest.testExpression(
+                "(true)",
+                "(expression ( (expression (constExpression true)) ))"
+        );
+    }
+
+    @Test
+    public void testParensPriority() throws Exception {
+        XParserTest.testExpression(
+                "1 * (2 + 3)",
+                "(expression (expression (constExpression 1)) (infixOperatorPrec1 *) (expression ( (expression (constExpression 2)) (infixOperatorPrec2 +) (expression (constExpression 3)) ))"
+        );
+    }
+}
