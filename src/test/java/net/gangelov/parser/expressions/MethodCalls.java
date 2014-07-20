faa89715cdebe0ca27068126b6502e04a07f3b2e
Georgy Angelov <georgyangelov@gmail.com>
2014-07-20 11:51:28 +0300

    Test code blocks

--- src/test/java/net/gangelov/parser/expressions/MethodCalls.java
+++ src/test/java/net/gangelov/parser/expressions/MethodCalls.java
@@ -93,4 +93,37 @@ public class MethodCalls {
                 "(expression (expression (constant 1)) . (methodID test) (actualArgumentList (expression (constant 1)) , (expression (expression (constant 2)) . (methodID test) (actualArgumentList ( (expression (constant 2)) ))) , (expression (constant 4))))"
         );
     }
+
+    @Test
+    public void testBlock() throws Exception {
+        XParserTest.testExpression(
+                "5.times:\n" +
+                    "Global.print 'Hello World!'\n" +
+                "end",
+
+                "(expression (expression (constant 5)) . (methodID times) (block : \\n (code (expression (classID Global) . (methodID print) (actualArgumentList (expression (constant 'Hello World!')))) \\n) end))"
+        );
+    }
+
+    @Test
+    public void testBlockAfterArguments() throws Exception {
+        XParserTest.testExpression(
+                "5.times 1, 'hai':\n" +
+                    "Global.print 1\n" +
+                "end",
+
+                "(expression (expression (constant 5)) . (methodID times) (actualArgumentList (expression (constant 1)) , (expression (constant 'hai'))) (block : \\n (code (expression (classID Global) . (methodID print) (actualArgumentList (expression (constant 1)))) \\n) end))"
+        );
+    }
+
+    @Test
+    public void testBlockWithArgumentList() throws Exception {
+        XParserTest.testExpression(
+                "5.times: index\n" +
+                    "Global.print index, ' times'\n" +
+                "end",
+
+                "(expression (expression (constant 5)) . (methodID times) (block : (blockArgumentNames (variable index)) \\n (code (expression (classID Global) . (methodID print) (actualArgumentList (expression (variable index)) , (expression (constant ' times')))) \\n) end))"
+        );
+    }
 }
